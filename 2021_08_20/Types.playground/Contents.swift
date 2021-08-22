d34ca47b9b61aee5526fbc35c9eee91c970a1576
Admin <vladislav.pratsko@gmail.com>
2021-08-22 12:30:46 +0300

    тест

--- 2021_08_20/Types.playground/Contents.swift
+++ 2021_08_20/Types.playground/Contents.swift
@@ -14,3 +14,4 @@ var result3 = doubleTest * Double(floatTest)
 var result4 = doubleTest * Double(integer16)
 var result5 = UInt32(integer8) + UInt32(integer16) + integer32
 var result6 = UInt32(Int32.max) + integer32
+//тест
