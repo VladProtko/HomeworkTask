96b4ce0e576e68b738dc4234e2692d2e6176407c
Admin <ghjnmrj123@gmail.com>
2021-08-25 14:58:07 +0300

    fix mistake

--- 2021_08_20_New/Types.playground/Contents.swift
+++ 2021_08_20_New/Types.playground/Contents.swift
@@ -4,17 +4,19 @@ import Foundation
 
 
 
-var integer32: UInt32 = UInt32.max
-var integer8: UInt8 = UInt8.max
-var integer64: Int64 = Int64.max
-var integer16: Int16 = Int16.max
+var integer32: UInt32 = UInt32.min
+var integer8: UInt8 = UInt8.min
+var integer64: Int64 = Int64.min
+var integer16: Int16 = Int16.min
 var doubleTest: Double = 0.5
 var floatTest: Float = 0.2
 var booltest: Bool = true
 
-var result1 = integer32 + UInt32(integer16)
+var result1 = Int64(integer32) + Int64(integer16)
 var result2 = integer64 - Int64(integer8)
-var result3 = doubleTest * Double(floatTest)
-var result4 = doubleTest * Double(integer16)
-var result5 = UInt32(integer8) + UInt32(integer16) + integer32
-var result6 = UInt32(Int32.max) + integer32
+var result3 = doubleTest / Double(floatTest)
+var result4 = doubleTest / Double(integer16)
+var result5 = Int64(integer8) + Int64(integer16) + Int64(integer32)
+var result6 = Int64(Int32.max) + Int64(integer32)
+
+
